4f023fe885eee99092e93456efda10a83e471eac
bismay <bismaypurkayastha5467@gmail.com>
2024-01-22 01:27:22 +0530

    constants relating to file upload form declared

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,14 @@
 export const BTC_TO_DOLLAR_RATE = 31883.72;
+
+export const MAX_FILE_SIZE = 5242880;
+export const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+export const ACCEPTED_SOURCE_FILE_TYPES = ["application/pdf"];
+
+export const comboBoxData = [{ label: "Sketch" }, { label: "lorem ispum" }];
+
+export const DAYS_IN_MONTH = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
